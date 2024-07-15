abc67e741fd18d28bb191ad326bcfe9484715dfc
Maxim Bogdanov <m.bogdanov@automacon.ru>
2024-07-15 13:06:52 +0500

    task1.4.1.1

---
+++ course1/4.variables/1.var_declaration/task1.4.1.1/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var firstNumber int = 0
+	var secondNumber int = 1
+	var thirdNumber int = 1
+	var fourthNumber int = 2
+	var fifthNumber int = 3
+	var sixthNumber int = 5
+	var seventhNumber int = 8
+	var eighthNumber int = 13
+
+	fmt.Println(firstNumber)
+	fmt.Println(secondNumber)
+	fmt.Println(thirdNumber)
+	fmt.Println(fourthNumber)
+	fmt.Println(fifthNumber)
+	fmt.Println(sixthNumber)
+	fmt.Println(seventhNumber)
+	fmt.Println(eighthNumber)
+}
