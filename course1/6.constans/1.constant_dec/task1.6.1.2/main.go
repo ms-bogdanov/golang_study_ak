ec9ced4d8d304c629e56d27f027b5136f83bafd5
Maxim Bogdanov <m.bogdanov@automacon.ru>
2024-07-15 16:32:29 +0500

    task1.6.1.2

---
+++ course1/6.constans/1.constant_dec/task1.6.1.2/main.go
@@ -0,0 +1,15 @@
+package main
+
+const (
+	Monday    int = 1
+	Tuesday   int = 2
+	Wednesday int = 3
+	Thursday  int = 4
+	Friday    int = 5
+	Saturday  int = 6
+	Sunday    int = 7
+)
+
+func main() {
+
+}
