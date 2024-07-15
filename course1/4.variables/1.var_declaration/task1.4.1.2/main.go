282a489e96c7cf55a8adcbcf6833a6e00151a2f8
Maxim Bogdanov <m.bogdanov@automacon.ru>
2024-07-15 13:11:36 +0500

    task1.4.1.2

---
+++ course1/4.variables/1.var_declaration/task1.4.1.2/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	firstNumber := "0"
+	secondNumber := "1"
+	thirdNumber := "1"
+	fourthNumber := "2"
+	fifthNumber := "3"
+	sixthNumber := "5"
+	seventhNumber := "8"
+	eighthNumber := "13"
+
+	fmt.Println(firstNumber + " " + secondNumber + " " + thirdNumber + " " + fourthNumber + " " + fifthNumber + " " + sixthNumber + " " + seventhNumber + " " + eighthNumber)
+}
